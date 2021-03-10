1f20ec2afde8bd466d747114524309b456ef9f04
Sachit71 <72592470+Sachit71@users.noreply.github.com>
2021-03-10 11:51:17 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,25 @@
+import sys
+import pyautogui
+x=pyautogui.prompt("Enter weight in Kg over here.")
+y=pyautogui.prompt("Enter height in centimetres over here.")
+#beta=pyautogui.prompt("Enter age her:")
+x=float(x)
+y=float(y)
+alpha=y/100
+z=pow(alpha,2.0)
+a=x/z
+z=float(z)
+a=float(a)
+#pyautogui.alert(a)
+if (a<18.5):
+    pyautogui.alert("Underweight")
+a=float(a)
+if 18.5<a<25:
+    pyautogui.alert("Normal")
+a=float(a)
+if a>30:
+    pyautogui.alert("Overweight")
+a=float(a)
+pyautogui.alert(a)
+
+
